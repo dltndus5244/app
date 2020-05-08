8c903289b9a1bab24e321abcc423f95ad451d666
dltndus5244 <dltndus5244@naver.com>
2020-05-08 15:57:15 +0900

    button

---
+++ Hello/app/src/main/java/org/techtown/hello/MainActivity.java
@@ -0,0 +1,32 @@
+package org.techtown.hello;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState); //부모 클래스에 있는 onCreate 함수 호출
+        setContentView(R.layout.activity_main); //화면에 무엇을 보여줄것인지 설정해주는 함수
+    }
+
+    public void onButton1Clicked(View v) {
+        Toast.makeText(this, "확인1 버튼이 눌렸어요.", Toast.LENGTH_LONG).show(); //TOAST함수는 작고 간단한 메시지를 잠깐 보여주는 역할
+    }
+
+    public void onButton2Clicked(View v) {
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://m.naver.com"));
+        startActivity(myIntent);
+    }
+
+    public void onButton3Clicked(View v) {
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1000-1000"));
+        startActivity(myIntent);
+    }
+}
